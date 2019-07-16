419d33157b162a4e910f74df110f1f42ea26df21
Aurelie <aurelie.benovici@gmail.com>
2019-07-16 22:44:10 +0200

    ok only unique value

--- models/scraper.js
+++ models/scraper.js
@@ -24,10 +24,10 @@ async function scrape() {
     let name = await data.$eval("h2.post-card-title", s =>
       s.textContent.trim()
     );
-    // let link = await data.$eval(
-    //   "a.post-card-image-link",
-    //   s => s.href
-    // );
+    let link = await data.$eval(
+      "a.post-card-image-link",
+      s => s.href
+    );
     let image = await data.$eval(
       "img.post-card-image",
       s => s.src
@@ -36,7 +36,7 @@ async function scrape() {
       s.textContent.trim()
     );
 
-    scrappedData.push({ name, title, image });
+    scrappedData.push({ name, title, image, link });
   }
 
   browser.close();
@@ -51,22 +51,38 @@ async function scrape() {
 async function runCron() {
   await scrape();
 
-  const beforeSaving = [];
-  // Array.prototype.diff = scrappedData
-  //   .filter(x => myDb.includes(x))
-  //   .push(beforeSaving);
+  // const beforeSaving = [];
 
   let dataValue = db.get("scrappedData").value();
   // .map(post => post.name);
 
-  datas = dataValue.concat(scrappedData);
-  console.log(datas.length);
-  let foo = new Map();
-  for (const c of datas) {
-    foo.set(c.name, c);
-  }
+  // datas = dataValue.concat(scrappedData);
+  // console.log(datas.length);
+  // let foo = new Map();
+  // for (const c of datas) {
+  //   foo.set(c.name, c);
+  // }
   // Array avec titre unique ancient et nouveau
-  let final = [...foo.values()];
+  // let final = [...foo.values()];
+  console.log("dataValue", dataValue.length);
+
+  function comparer(otherArray) {
+    return function(current) {
+      return (
+        otherArray.filter(function(other) {
+          return other.name == current.name;
+        }).length == 0
+      );
+    };
+  }
+  const dataToAdd = scrappedData.filter(
+    comparer(dataValue)
+  );
+
+  console.log("RESULT ", dataToAdd.length);
+  // dataValue.length === final.length
+  //   ? "Pas de nouveau post "
+  //   : final;
   // ************TO DO ********* *
   // Verifier dataValue.length et final.length
   //   si length est égal alors pas de nouveau post
@@ -74,9 +90,13 @@ async function runCron() {
   // const newState = { scrappedData };
   // db.setState(newState);
 
-  console.log("TEST COMBINED", final.length);
+  // console.log("TEST COMBINED", final.length);
   // console.log("RESULT", result);
-
+  await db
+    .get("scrappedData")
+    .push(dataToAdd)
+    .write();
+  console.log("Done");
   // await db
   //   .get("scrappedData")
   //   .push({ result })
